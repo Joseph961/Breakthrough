6635a9f1420b38e0e209b5bd9bf45c592d0a439a
unknown <joseph.matthews@mirage.students.cset.oit.edu>
2015-05-21 11:33:09 -0700

    Signed-off-by: unknown <joseph.matthews@mirage.students.cset.oit.edu>

---
+++ Breakout/Assets/BallMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+	//public float speed;
+	Rigidbody rb;
+
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		//	mouse = Input.mousePosition.x;
+		Vector3 movement = new Vector3 (0.0f, -10.0f, 0.0f);
+		rb.AddForce (movement * 100);
+		rb.velocity = (movement * -1);
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	//	float moveHorizontal = Input.GetAxis ("Horizontal");
+		
+	//	Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+		
+	//	rb.AddForce (movement * speed);
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Brick")) 
+		{
+			other.gameObject.SetActive (false);
+
+
+		}
+	}
+
+}
---
+++ Breakout/Assets/MovePaddle.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePaddle : MonoBehaviour {
+
+	bool right =false;
+	bool left = false;
+	public float speed;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	//	mouse = Input.mousePosition.x;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+
+		if (moveHorizontal > 0 && !right) 
+		{
+						right = true;
+						transform.Rotate (new Vector3 (0, 0, -15));
+			if (left){
+				left = false;
+				transform.Rotate (new Vector3 (0, 0, -15));
+			}
+		} else if (moveHorizontal < 0 && !left) {
+			left = true;
+			transform.Rotate (new Vector3 (0, 0, 15));
+			if(right){
+				transform.Rotate (new Vector3 (0, 0, 15));
+				right = false;
+			}
+		} else {
+
+
+		}
+
+
+		if ((rb.position.x < 7 && moveHorizontal > 0) ||(rb.position.x > -7 && moveHorizontal < 0)) {
+						Vector3 movement = new Vector3 (moveHorizontal + rb.position.x, rb.position.y, rb.position.z);
+			rb.MovePosition (movement);
+				}
+
+		//if(moveHorizontal > 0)
+
+		//rb.AddForce (movement * speed);
+
+	
+	}
+}
